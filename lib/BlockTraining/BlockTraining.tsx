989b5dae13780b9914ae3236e8bc5aff18c77e53
Kazerlelutin <social@studiokami.fr>
2021-09-20 11:07:54 +0200

    fix(index): :lipstick: training

--- lib/BlockTraining/BlockTraining.tsx
+++ lib/BlockTraining/BlockTraining.tsx
@@ -1,10 +1,14 @@
 import { useInView } from "react-intersection-observer";
 import classes from "./BlockTraining.module.css";
+import useIsMobile from "../../utils/useIsMobile";
 
 export default function BlockTraining() {
   const { ref, inView } = useInView({
-    threshold: 0,
-  });
+      threshold: 0,
+    }),
+    ismobile = useIsMobile();
+
+    console.log(ismobile)
   return (
     <div className={classes.container} ref={ref} data-inview={inView}>
       <h2 className={classes.title}>
@@ -26,7 +30,33 @@ export default function BlockTraining() {
             <div className={classes.city}>76 150 MAROMME</div>
           </div>
         </div>
-        <div className={classes.trainings}>
+        {ismobile ? (
+   <div className={classes.trainingsMobile}>
+   <div className={classes.training}>
+     <div className={classes.day}>Vendredi</div>
+     <div className={classes.time}>19h30 - 22h00</div>
+     <div className={classes.address}>
+       <div className={classes.level}>Ouvert à tous</div>
+       <div className={classes.stadium}>Gymnase Jesse OWENS</div>
+       <div className={classes.street}>rue Denis Papin</div>
+       <div className={classes.details}>Parc de SIGNA</div>
+       <div className={classes.city}>76 153 MAROMME</div>
+     </div>
+   </div>
+   <div className={classes.training}>
+     <div className={classes.day}>Dimanche</div>
+     <div className={classes.time}>17h00 - 19h30</div>
+     <div className={classes.address}>
+       <div className={classes.level}>Ouvert à tous</div>
+       <div className={classes.stadium}>Gymnase Jesse OWENS</div>
+       <div className={classes.street}>rue Denis Papin</div>
+       <div className={classes.details}>Parc de SIGNA</div>
+       <div className={classes.city}>76 153 MAROMME</div>
+     </div>
+   </div>
+ </div>
+        ) : (
+          <div className={classes.trainings}>
           <div className={classes.day}>Vendredi</div>
           <div className={classes.day}>Dimanche</div>
           <div className={classes.time}>19h30 - 22h00</div>
@@ -39,6 +69,8 @@ export default function BlockTraining() {
             <div className={classes.city}>76 153 MAROMME</div>
           </div>
         </div>
+          
+        )}
       </div>
     </div>
   );
